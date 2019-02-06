790d09cc094cabe1cac3684fab8a92fd04594b57
Alfian Ridwan <hello@alfianridwan.com>
2019-02-06 13:49:48 +0800

    Rename layout to Layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 const aboutPage = () => <Layout />;
 
